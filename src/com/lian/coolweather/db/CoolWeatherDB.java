97c16e84a0bafbe725a1b8c4bf894ec6379982dd
Lian <lian070200720@126.com>
2016-03-31 09:00:06 +0800

    新增数据库帮助类，以及各表对应的实体类。

---
+++ src/com/lian/coolweather/db/CoolWeatherDB.java
@@ -0,0 +1,5 @@
+package com.lian.coolweather.db;
+
+public class CoolWeatherDB {
+
+}
---
+++ src/com/lian/coolweather/model/Province.java
@@ -0,0 +1,31 @@
+package com.lian.coolweather.model;
+
+public class Province {
+	private int id;
+	private String provinceName;
+	private String provinceCode;
+	
+	public int getId(){
+		return id;
+	}
+	
+	public void setId(int id){
+		this.id = id;
+	}
+	
+	public String getProvinceName(){
+		return this.provinceName;
+	}
+	
+	public void setProvinceName(String provinceName){
+		this.provinceName = provinceName;
+	}
+	
+	public String getProvinceCode(){
+		return this.provinceCode;
+	}
+	
+	public void setProvinceCode(String provinceCode){
+		this.provinceCode = provinceCode;
+	}
+}
